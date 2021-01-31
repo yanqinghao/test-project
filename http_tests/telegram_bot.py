958ac4267221cfa743b734cafcd39a8f5d7d87da
yanqinghao <woshiyanqinghao@gmail.com>
2021-01-31 12:42:25 +0800

    telegram bot api

---
+++ http_tests/telegram_bot.py
@@ -0,0 +1,15 @@
+import requests
+
+send_message_url = 'https://api.telegram.org/bot<token>/sendMessage'
+updates_url = 'https://api.telegram.org/bot<token>/getUpdates'
+
+token = open('private/bot_token').readline()
+send_message_url = send_message_url.replace('<token>', token)
+updates_url = updates_url.replace('<token>', token)
+
+chat_id = open('private/chat_id').readline()
+data = {"chat_id": int(chat_id), "text": "小臭子，你好"}
+
+r = requests.post(send_message_url, data=data)
+
+print(r.content)
