e73caced506a988f573cba66e66d22b689f5bdc5
YanQinghao <woshiyanqinghao@gmail.com>
2021-03-04 18:35:32 +0800

    read sql

--- pandas_tests/read.py
+++ pandas_tests/read.py
@@ -1,4 +1,16 @@
+import random
+import sqlite3
 import pandas as pd
 
 df = pd.read_excel("private/表格1.xlsx", index_col=0)
 print(df)
+
+cnx = sqlite3.connect("private/2-100.db")
+df1 = pd.read_sql("SELECT * FROM data", cnx)
+print(df1)
+
+df1["simple_content"] = df1["content"].str.replace("<.+?>", " ")
+classes = ["simulation", "hardware", "edge_computing"]
+df1["label"] = random.choices(classes, k=len(df1))
+df1.to_sql(name="data", con=cnx, index=False, if_exists='replace')
+df1.to_csv("private/2-100.csv", index=False)
