006e79a0fd4d1b39e6e7adc1e2d38bddb511f8af
YanQinghao <woshiyanqinghao@gmail.com>
2021-11-26 13:37:24 +0800

    fix

--- http_tests/post.py
+++ http_tests/post.py
@@ -1,9 +1,9 @@
 import requests
 
-url = "http://localhost:8080/train"
+url = "https://sp.xuelangyun.com/proxr/shanglu/61812/14747c80420911eca21d75a6433baa8f/8899/checkCode"
 
-data = {"path": "/data/engine", "label": "engine"}
+data = {"code": "111111"}
 
-r = requests.post(url, data=data)
+r = requests.post(url, json=data)
 
 print(r.content)
---
+++ video/cut.sh
@@ -0,0 +1,1 @@
+ffmpeg -ss 0:0:40 -to 0:48:50 -i /home/yanqinghao/code/test-project/video/train.mp4 -vcodec copy -acodec copy output.mp4
